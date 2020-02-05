1474de99c3a9f67758014e96128e8a24fd36d18e
dravesstorm <dravesstorm@users.github.com>
2020-02-06 00:51:54 +0200

    DelHero

--- Assets/Scripts/SpawnPlatform.cs
+++ Assets/Scripts/SpawnPlatform.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SpawnPlatform : MonoBehaviour
 {
     public GameObject platform;
+
     private float indent = 1f;
     // Start is called before the first frame update
     void Start()
