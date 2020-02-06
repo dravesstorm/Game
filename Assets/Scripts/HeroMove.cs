eb85ae0ce6914432e54ace19776270ca637ee78a
dravesstorm <dravesstorm@users.github.com>
2020-02-06 05:02:41 +0200

    Revert "Merge branch 'Sergei' into Pavel"
    This reverts commit f2340144c684b4556bb2af59718fb6f729ae4f59, reversing
    changes made to 0217ddfaf0217dddbe66c5c0aa2106abc452b8b0.

---
+++ Assets/Scripts/HeroMove.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroMove : MonoBehaviour
+{
+    public float speed;
+    public float addForce;
+    public KeyCode leftButton = KeyCode.A;
+    public KeyCode rightButton = KeyCode.D;
+    public KeyCode addForceButton = KeyCode.W;
+    public static bool isFacingRight = true;
+    private Vector3 direction;
+    private float horizontal;
+    private Rigidbody2D body;
+    public static bool canJump;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        body.fixedAngle = true;
+        canJump = true;
+    }
+
+
+    void FixedUpdate()
+    {
+        body.AddForce(direction * body.mass * speed/2);
+
+        if (Input.GetKey(addForceButton) && canJump)
+        {
+            body.velocity = new Vector2(0, addForce);
+            canJump = false;
+        }
+    }
+
+    void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+
+    
+    void Update()
+    {
+        if (Input.GetKey(leftButton)) horizontal = -1;
+             else if (Input.GetKey(rightButton)) horizontal = 1; 
+                     else horizontal = 0;
+
+        direction = new Vector2(horizontal, 0);
+        if (horizontal > 0 && !isFacingRight ) Flip(); 
+            else if (horizontal < 0 && isFacingRight) Flip();
+    }
+}
---
+++ Assets/Scripts/SpawnPlatform.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPlatform : MonoBehaviour
+{
+    public GameObject platform;
+    public GameObject groundCollPlatform;
+
+    private float indent = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(CanCreatePlatform());
+       
+    }
+    IEnumerator CanCreatePlatform()
+    {
+        float sin = 0f;
+        for (int i = -8; i < 500000000; i++)
+        {
+            Instantiate(platform, new Vector3(indent * i, -5 + 2*Mathf.Sin(sin), 0), Quaternion.identity);
+            Instantiate(groundCollPlatform, new Vector3(indent * i, -5 + 2 * Mathf.Sin(sin), 0), Quaternion.identity);
+            sin += 0.1f;
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+
+}
