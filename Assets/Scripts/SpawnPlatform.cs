a25b9357f0a9b52595d56efa857219d2fd6e321a
WebProjectGroup01 <60502853+WebProjectGroup01@users.noreply.github.com>
2020-02-07 09:47:48 +0200

    AddEnemy

--- Assets/Scripts/SpawnPlatform.cs
+++ Assets/Scripts/SpawnPlatform.cs
@@ -6,9 +6,8 @@ public class SpawnPlatform : MonoBehaviour
 {
     public GameObject platform;
     public GameObject groundCollPlatform;
-
+    public GameObject enemy;
     private float indent = 1f;
-    // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(CanCreatePlatform());
@@ -17,10 +16,15 @@ public class SpawnPlatform : MonoBehaviour
     IEnumerator CanCreatePlatform()
     {
         float sin = 0f;
-        for (int i = -8; i < 500000000; i++)
+        for (int i = -10; i < 5000; i++)
         {
             Instantiate(platform, new Vector3(indent * i, -5 + 2*Mathf.Sin(sin), 0), Quaternion.identity);
             Instantiate(groundCollPlatform, new Vector3(indent * i, -5 + 2 * Mathf.Sin(sin), 0), Quaternion.identity);
+            if(i % 49 == 0)
+            {
+                Instantiate(enemy, new Vector3(indent * i, (-5 + 2 * Mathf.Sin(sin))+1, 0), Quaternion.identity);
+
+            }
             sin += 0.1f;
             yield return new WaitForSeconds(0.01f);
         }
