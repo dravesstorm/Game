c3e9c8f76c85780b1ac896071de4216d402a58c7
WebProjectGroup <WebProjectGroup@i.ua>
2020-04-17 14:15:16 +0300

    fix

---
+++ Assets/Scripts/start/start.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class start : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        Application.LoadLevel("SampleScene");
+    }
+}
